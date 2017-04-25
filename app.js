415d736f9509c7b6d1060bbc42d76e5c0d37a3fe
Ketevann <ketevan.tsin@gmail.com>
2017-04-25 10:17:52 -0400

    starting a server, work in progress

--- app.js
+++ app.js
@@ -5,6 +5,31 @@ var server = app.listen('3000', function(){
   console.log("Server listening")
 })
 
+app.use(function(req,res, next){
+  console.log(req.method +" "+req.url)
+  next();
+});
+app.use('/special/*',function(req,res, next){
+  console.log("you reached the special area");
+});
+
+
 app.get('/', function(req,res, next){
-  res.send("Welcome!!")
-})
+console.log("pr!!!");
+  res.send("<h1>bla!!</h1>")
+
+
+});
+
+app.get('/special', function(req,res, next){
+  console.log("special");
+  next();
+
+
+});
+
+app.get('/news', function(req,res, next){
+     res.send("Welcome to news!!!")
+});
+
+
